39e9f1438d635034fae16ef2744b3b5539caee65
Maria Clara <mariaclara2016085@gmail.com>
2026-05-15 09:13:00 -0300

    feat: implementando o service filter-episodes

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -1,11 +1,22 @@
 import fs from "fs"; //file system para ler e escrever em arquivos de sistema
 import path from "path"; //le diretorios especificos
 import { Podcast } from "../models/podcast-models";
+import { json } from "stream/consumers";
 
 const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
-export const repositoryPodcast = async (): Promise<Podcast[]> => {
+//esta função tme opção de receber ou não um parâmetro (?)
+export const repositoryPodcast = async (
+  podcastName?: String,
+): Promise<Podcast[]> => {
   const data = fs.readFileSync(pathData, "utf-8");
-  const jsonFile = JSON.parse(data); //convertendo o arquivo em json
+  let jsonFile = JSON.parse(data); //convertendo o arquivo em json
+
+  if (podcastName) {
+    jsonFile = jsonFile.filter(
+      (podcast: Podcast) => podcast.podcastName === podcastName,
+    );
+  }
+
   return jsonFile;
 };
